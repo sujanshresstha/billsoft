a0b4bf15cce9ebfa344db01a7ffadbd273f54b4a
Sujan Shrestha <sujanshresstha@gmail.com>
2025-07-05 17:07:33 +0900

    initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/test/java/com/sujanshrestha/billingsoftware/BillingsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujanshrestha.billingsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
