2bfbb59f97c7b1c6632104464f1067ebf69f3e92
Sujan Shrestha <sujanshresstha@gmail.com>
2025-07-05 20:13:17 +0900

    add CategoryEntity class for category management

---
+++ src/main/java/com/sujanshrestha/billingsoftware/entity/CategoryEntity.java
@@ -0,0 +1,34 @@
+package com.sujanshrestha.billingsoftware.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "tbl_category")
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String categoryId;
+    @Column(unique = true)
+    private String name;
+    private String description;
+    private String bgColor;
+    private String imgUrl;
+    @CreationTimestamp
+    private Timestamp createdAt;
+    @CreationTimestamp
+    private Timestamp updatedAt;
+}
